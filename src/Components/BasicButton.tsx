ed6c3c83695444a487dc7592802c90fb11344c40
Axel Rodriguez-Leon <axelarl@udel.edu>
2025-03-31 22:01:15 -0400

    Finished first iteration of ToBasic  component

--- src/Components/BasicButton.tsx
+++ src/Components/BasicButton.tsx
@@ -1,6 +1,18 @@
 import React, {useState} from "react";
 import {Button} from "react-bootstrap";
 
-export function toBasic(): React.JSX.Element{
-    
+export function ToBasic(): React.JSX.Element{
+    const [page, setPage] = useState<string>("");
+    return(
+        <span>
+            <Button
+                onClick={() =>{
+                    setPage("BasicQuestions.tsx");
+                }}
+                >
+                    Basic Quiz
+                </Button>
+                to {page}.
+        </span>
+    );
 }
