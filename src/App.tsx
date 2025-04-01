6cbff476edd1290c7cc5095b56c481ff3212b42d
Alex Chambers <alexcham@udel.edu>
2025-04-01 15:30:16 -0400

    Attempting merge #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import { Button, Form } from 'react-bootstrap';
+import stockImage from './careerquizwithbackground.png';
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
 let keyData = "$OPENAI_API_KEY";
@@ -25,24 +26,45 @@ function App() {
   }
   return (
     <div className="App">
-      <p>
-          Shaina Zaccagnino, Axel Rodriguez-Leon, Alexander Chambers
-        </p>
-      <header className="App-header">
-        <p>
-          Career Helpi
-        </p>
-        <header className ="Questions">
+      <Button className="take-quiz-button">Take our free career quiz now!</Button>
+      <div className="fade-strip">Career Helpi Quiz</div>
+      <header className="App-header"></header>
+      {/* uncomment later when questions are finished */}
+      {/* <header className ="Questions"></header> */}
         <p className ="basic">Not sure where to start? Answer a few small questions to get some starting points on a career path.</p> 
-        <p className ="detailed"> Have a few ideas of what you want? Take this quiz to help narrow those career choices down!</p>
-       </header>
-      </header>
-      <Form>
-        <Form.Label>API Key:</Form.Label>
+        <p className ="detailed"> Have a few ideas of what you want? Take this quiz to help narrow those career choices down!</p>      
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        </a>
+      <Form className="api-form-container">
+        <Form.Label style={{ fontFamily: 'Verdana, sans-serif', color: 'white', fontSize: '20px', }}>API Key:</Form.Label>
         <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
         <br></br>
-        <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
-      </Form>
+          <Button style={{
+          backgroundColor: '#d3dced',
+          color: 'black',
+          fontSize: '16px',
+          padding: '10px 20px',
+          border: 'none',
+          borderRadius: '20px',
+          cursor: 'pointer'
+          }} className="Submit-Button" onClick={handleSubmit}>Submit</Button>
+          </Form>
+          <img 
+          src={stockImage}
+          alt="stockimage" 
+          style={{
+            position: 'fixed',
+            bottom: 0,
+            right: 10,
+            width: '450px',
+            height: 'auto',
+          }} 
+        />
     </div>
   );
 }
