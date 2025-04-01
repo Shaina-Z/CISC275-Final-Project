d6eefd5ea4e513137510d0b8fbb74ccb72c1d87d
Axel Rodriguez-Leon <axelarl@udel.edu>
2025-03-31 23:09:40 -0400

    Possibly finished BasicQuestions page

--- src/Pages/BasicQuestions.tsx
+++ src/Pages/BasicQuestions.tsx
@@ -2,3 +2,51 @@ import React, { useState } from 'react';
 import './App.css';
 import { Button, Form } from 'react-bootstrap';
 
+const Q1ANSWER = [
+    "Choice A",
+    "Choice B",
+    "Choice C",
+    "Choice D"
+]
+const Q2ANSWER = [
+    "Choice A",
+    "Choice B",
+    "Choice C",
+    "Choice D"
+]
+const Q3ANSWER = [
+    "Choice A",
+    "Choice B",
+    "Choice C",
+    "Choice D"
+]
+
+export function basicQuestions(): React.JSX.Element {
+    const [answer, setAnswer] = useState<string>(Q1ANSWER[0]);
+    
+    return(
+    <div className = "Basic Questions">
+        <h3>Basic Quiz Questions</h3>
+        <Form.Group controlId={"Colors"}>
+                    {Q1ANSWER.map((ananswer) => (
+                        <Form.Check
+                            inline
+                            type={"radio"}
+                            name={"response"}
+                            key={ananswer}
+                            value={ananswer}
+                            label={
+                                <span style={{ backgroundColor: ananswer }}>
+                                    {ananswer}
+                                </span>
+                            }
+                            onChange={(e) => {
+                                setAnswer(e.target.value);
+                            }}
+                            checked={answer === ananswer}
+                        />
+                    ))}
+                </Form.Group>
+        </div>
+    )
+}
