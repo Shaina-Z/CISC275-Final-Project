dd653a04ee87c71600849d4048779f4eb88bd199
Shaina <shainaz@udel.edu>
2025-04-15 10:26:39 -0400

    added more questions

--- src/Pages/BasicQuestions.tsx
+++ src/Pages/BasicQuestions.tsx
@@ -21,12 +21,41 @@ const Q3ANSWER = [
     "Choice D"
 ]
 
+const Q4ANSWER = [
+    "Choice A",
+    "Choice B",
+    "Choice C",
+    "Choice D"
+]
 
+const Q5ANSWER = [
+    "Choice A",
+    "Choice B",
+    "Choice C",
+    "Choice D"
+]
 
+const Q6ANSWER = [
+    "Choice A",
+    "Choice B",
+    "Choice C",
+    "Choice D"
+]
+
+const Q7ANSWER = [
+    "Choice A",
+    "Choice B",
+    "Choice C",
+    "Choice D"
+]
 export function BasicQuestions(): React.JSX.Element{
     const [answer1, setAnswer1] = useState<string>(Q1ANSWER[0]);
     const [answer2, setAnswer2] = useState<string>(Q2ANSWER[0]);
     const [answer3, setAnswer3] = useState<string>(Q3ANSWER[0]);
+    const [answer4, setAnswer4] = useState<string>(Q4ANSWER[0]);
+    const [answer5, setAnswer5] = useState<string>(Q5ANSWER[0]);
+    const [answer6, setAnswer6] = useState<string>(Q6ANSWER[0]);
+    const [answer7, setAnswer7] = useState<string>(Q7ANSWER[0]);
     const [progress,setProgress]=useState<number>(0)
 
     function UpdateProgress(){
@@ -90,6 +119,82 @@ export function BasicQuestions(): React.JSX.Element{
                                      UpdateProgress();
                                 }}
                                 checked={answer3 === ananswer3}
+                            />
+                        ))}
+                </Form.Group>
+                <h3>Question 4</h3>
+                    <Form.Group controlId={"Question 3"}>
+                            {Q4ANSWER.map((ananswer4) => (
+                                <Form.Check
+                                inline
+                                type={"radio"}
+                                name={"response4"}
+                                key={ananswer4}
+                                value={ananswer4}
+                                label={ananswer4}
+                                onChange={(e) => {
+                                    setAnswer4(e.target.value
+                                     );
+                                     UpdateProgress();
+                                }}
+                                checked={answer4 === ananswer4}
+                            />
+                        ))}
+                </Form.Group>
+                <h3>Question 5</h3>
+                    <Form.Group controlId={"Question 5"}>
+                            {Q5ANSWER.map((ananswer5) => (
+                                <Form.Check
+                                inline
+                                type={"radio"}
+                                name={"response5"}
+                                key={ananswer5}
+                                value={ananswer5}
+                                label={ananswer5}
+                                onChange={(e) => {
+                                    setAnswer5(e.target.value
+                                     );
+                                     UpdateProgress();
+                                }}
+                                checked={answer5 === ananswer5}
+                            />
+                        ))}
+                </Form.Group>
+                <h3>Question 6</h3>
+                    <Form.Group controlId={"Question 6"}>
+                            {Q6ANSWER.map((ananswer6) => (
+                                <Form.Check
+                                inline
+                                type={"radio"}
+                                name={"response6"}
+                                key={ananswer6}
+                                value={ananswer6}
+                                label={ananswer6}
+                                onChange={(e) => {
+                                    setAnswer6(e.target.value
+                                     );
+                                     UpdateProgress();
+                                }}
+                                checked={answer6 === ananswer6}
+                            />
+                        ))}
+                </Form.Group>
+                <h3>Question 7</h3>
+                    <Form.Group controlId={"Question 7"}>
+                            {Q7ANSWER.map((ananswer7) => (
+                                <Form.Check
+                                inline
+                                type={"radio"}
+                                name={"response7"}
+                                key={ananswer7}
+                                value={ananswer7}
+                                label={ananswer7}
+                                onChange={(e) => {
+                                    setAnswer7(e.target.value
+                                     );
+                                     UpdateProgress();
+                                }}
+                                checked={answer7 === ananswer7}
                             />
                         ))}
                 </Form.Group>
