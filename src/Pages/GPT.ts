bb23ad80e63ac3408f395c93eb9318d6e6beaf28
Shaina <shainaz@udel.edu>
2025-04-27 14:26:23 -0400

    remade gpt into a new file

---
+++ src/Pages/GPT.ts
@@ -0,0 +1,30 @@
+import OpenAI from "openai";
+
+async function genResponse(responses: string) {
+    const APIKey = localStorage.getItem("MYKEY");
+    if(APIKey == null){
+        return("Error: You did not submit your API Key.")
+    }
+    
+    try {
+    const stringKey = JSON.stringify(APIKey).slice(3,-3);
+    const openai = new OpenAI({apiKey: stringKey, dangerouslyAllowBrowser: true});
+        const completion = await openai.chat.completions.create({
+            messages: [{"role": "system", "content": "You are a helpful  career assistant."},
+            {"role": "user", "content": "Based on these answers, what career would you recommend for this person? \n" 
+                + responses 
+                + "\n Additionally, what companies would you recommend?"},],
+            
+            model: "gpt-4o",
+  })
+  
+  console.log(completion.choices[0].message.content);
+  const response: string = completion.choices[0].message.content ||  "";
+  return(response)
+
+} catch (error){
+    return "Error: Invalid API Key.";
+}
+};
+
+export default genResponse;
