760a71748cd3794e06265214bce8076ee674d4ba
Shaina <shainaz@udel.edu>
2025-05-07 10:34:17 -0400

    adding another function for communication with chatGPT

--- src/Pages/GPT.ts
+++ src/Pages/GPT.ts
@@ -26,5 +26,28 @@ async function genResponse(responses: string) {
     return "Error: Invalid API Key.";
 }
 };
+export async function chatResponse(responses: string) {
+    const APIKey = localStorage.getItem("MYKEY");
+    if(APIKey == null){
+        return("Error: You did not submit your API Key.")
+    }
+    
+    try {
+    const stringKey = JSON.stringify(APIKey).slice(3,-3);
+    const openai = new OpenAI({apiKey: stringKey, dangerouslyAllowBrowser: true});
+        const completion = await openai.chat.completions.create({
+            messages: [{"role": "system", "content": "You are a helpful  career assistant."},
+            {"role": "user", "content": responses},],
+            
+            model: "gpt-4o",
+  })
+  
+  console.log(completion.choices[0].message.content);
+  const response: string = completion.choices[0].message.content ||  "";
+  return(response)
 
+} catch (error){
+    return "Invalid input.";
+}
+};
 export default genResponse;
